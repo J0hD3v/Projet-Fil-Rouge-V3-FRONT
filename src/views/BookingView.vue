2e48d25f9a7004d5ebd60314add758be74a7588e
J0hD3v <johanfievet@orange.fr>
2025-06-16 01:53:41 +0200

    Modifs de test sur les events du calendrier

--- src/views/BookingView.vue
+++ src/views/BookingView.vue
@@ -62,20 +62,50 @@
         },
         events: [
             {
-            id: 1,
-            title: 'Tournois mensuel',
-            start: '2025-06-19',
-            end: '2025-06-19',
-            description: 'Places restantes: ' + '7',
-            location: 'terrain 3'
+                id: 1,
+                title: 'Tournois mensuel',
+                start: '2025-06-20 09:00',
+                end: '2025-06-20 18:00',
+                description: 'Places restantes: ' + 7 + ' - Arriver 15min avant le début svp',
+                location: 'terrain 3',
+                calendarId: 'tournois'
             },
             {
-            id: 2,
-            title: 'Event 2',
-            start: '2025-06-20 12:00',
-            end: '2025-06-20 13:00',
+                id: 2,
+                title: 'Cours collectif',
+                start: '2025-06-20 12:00',
+                end: '2025-06-20 13:00',
+                description: 'Places restantes: ' + 3,
+                location: 'Terrain 1',
+                people: ['Coach: Senpai']
             },
+            {
+                id: 3,
+                title: 'Journée d\'inauguration',
+                start: '2025-06-19',
+                end: '2025-06-19',
+                description: 'Pour fêter l\'ouverture du club, bienvenue à tout le monde !',
+                calendarId: 'special'
+            }
         ],
+        calendars: {
+            tournois: {
+                colorName: 'tournois',
+                lightColors: {
+                    main: '#1c7df9',    // bordure
+                    container: '#d2e7ff',   // fond
+                    onContainer: '#002859', // texte
+                }
+            },
+            special: {
+                colorName: 'special',
+                lightColors: {
+                    main: 'red',    // bordure
+                    container: '#e7a8a8',   // fond
+                    onContainer: '#860000', // texte
+                }
+            }
+        },
         callbacks: {
             onEventClick(calendarEvent) {
                 // console.log('onEventClick', calendarEvent);
