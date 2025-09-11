1f1b6de109aa6049cae6c329f5f3bb18b15701b6
J0hD3v <johanfievet@orange.fr>
2025-09-11 09:24:55 +0200

    enlevement du test redirection + adaptation navbar icones

--- src/components/layout/NavBar3.vue
+++ src/components/layout/NavBar3.vue
@@ -7,8 +7,11 @@
         <template #item="{ item, props, hasSubmenu, root }">
             <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
                 <a v-ripple :href="href" v-bind="props.action" @click="navigate">
-                    <span :class="item.icon" style="font-size: xx-large" />
-                    <span>{{ item.label }}</span>
+                    <section class="d-flex flex-column align-items-center justify-center">
+                        <span :class="item.icon" style="font-size: xx-large" />
+                        <span>{{ item.label }}</span>
+                    </section>
+                    <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
                 </a>
             </router-link>
             <!-- <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
